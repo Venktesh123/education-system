b88fd7880165c96f1586a235fa5c620c9d9426fb
Venktesh123 <8303531530venktesh@gmail.com>
2025-07-16 14:14:36 +0530

    changed-origin

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5173",
       "http://localhost:3001",
+      "https://www.onecampus.online",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: [
